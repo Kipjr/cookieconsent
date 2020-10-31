31e072f30cf2d520a349d1ef5dc1dbf67b7e86da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-10-31 22:28:48 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'D\'acord';
+$lang['Button text'] = 'Text del botó';
+$lang['Common configuration'] = 'Configuració comuna';
+$lang['Full screen overlay'] = 'Pantalla completa sobreposada en lloc d\'una barra a la part inferior de la pantalla';
+$lang['Fullscreen'] = 'Pantalla completa';
+$lang['Message text'] = 'Text del missatge';
+$lang['More Info'] = 'Més informació';
+$lang['Save Settings'] = 'Desa els paràmetres';
+$lang['URL'] = 'URL';

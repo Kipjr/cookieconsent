81ff079f70dba17d29790107cb019435eaf6e818
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-02 10:50:49 +0100

    Update da_DK, thanks to Kaare

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -6,3 +6,12 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Anvend sessioncookie i stedet for vedvarende (persistent) cookie';
+$lang['Full screen overlay'] = 'Fuldskærmsoverlay i stedet for en bjælke i bunden af skærmen';
+$lang['Accept'] = 'Accepter';
+$lang['Button text'] = 'Knaptekst';
+$lang['Common configuration'] = 'Generel opsætning';
+$lang['Fullscreen'] = 'Fuldskærm';
+$lang['Message text'] = 'Meddelelsestekst';
+$lang['More Info'] = 'Flere oplysninger';
+$lang['Save Settings'] = 'Gem indstillinger';

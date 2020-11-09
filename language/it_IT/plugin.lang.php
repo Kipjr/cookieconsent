36fb5817d775cf05a9920d546ba6f7f1dc5d5cef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-09 12:16:57 +0100

    Update it_IT, thanks to rio

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use session cookie'] = 'Usa cookie di sessione invece di cookie persistenti.';
+$lang['Accept'] = 'Accetto';
+$lang['Button text'] = 'Etichetta URL';
+$lang['Common configuration'] = 'Configurazione comune';
+$lang['Full screen overlay'] = 'Sovrapposizione a schermo intero invece di una barra nella parte inferiore dello schermo.';
+$lang['Fullscreen'] = 'A schermo intero';
+$lang['Message text'] = 'testo del messaggio';
+$lang['More Info'] = 'Maggiori informazioni';
+$lang['Save Settings'] = 'Salva le impostazioni';
+$lang['URL'] = 'URL';

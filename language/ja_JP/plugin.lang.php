d5947e9c5a7f2959121ae993f271efa418de1031
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-13 05:53:00 +0100

    Update ja_JP, thanks to ryoon

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = '同意する';
+$lang['Button text'] = 'ボタンのテキスト';
+$lang['Common configuration'] = '共通設定';
+$lang['Full screen overlay'] = 'スクリーンの下部にバーを表示するのではなく、フルスクリーンで表示する';
+$lang['Fullscreen'] = 'フルスクリーン';
+$lang['Message text'] = 'メッセージテキスト';
+$lang['More Info'] = '追加の情報';
+$lang['Save Settings'] = '変更を保存する';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = '永続的なクッキーではなく、セッションクッキーを利用する';

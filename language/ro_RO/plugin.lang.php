f9aec0067877c067d9ec73532f66add13f96d79a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-14 20:40:19 +0100

    Update ro_RO, thanks to Jerome-PH

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Accept';
+$lang['Button text'] = 'Text buton';
+$lang['Common configuration'] = 'Configurație comună';
+$lang['Message text'] = 'Mesaj text';
+$lang['More Info'] = 'Mai multe informații';
+$lang['Save Settings'] = 'Salvează setările';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Folosiți cookie de sesiune în loc de cookie persistent.';

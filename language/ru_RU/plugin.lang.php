487784e0233c11aec2a57b7e671f0fceea939010
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-01 06:52:12 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Принять';
+$lang['Button text'] = 'Текст кнопки';
+$lang['Common configuration'] = 'Общая конфигурация';
+$lang['Full screen overlay'] = 'Полноэкранное отображение вместо полоски в нижней части экрана.';
+$lang['Fullscreen'] = 'Полноэкранный режим';
+$lang['Message text'] = 'Текст сообщения';
+$lang['More Info'] = 'Дополнительная информация';
+$lang['Save Settings'] = 'Сохранение установок';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Используйте сессионные куки вместо постоянных файлов cookie.';

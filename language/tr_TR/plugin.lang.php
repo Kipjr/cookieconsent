91647b90530b56051ec88f7038d4cc087a455372
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-12-18 18:23:45 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Full screen overlay'] = 'Ekranın altındaki çubuk yerine tam ekran yer paylaşımı.';
+$lang['More Info'] = 'Daha fazla Bilgi';
+$lang['Use session cookie'] = 'Kalıcı çerezler yerine oturumda çerez kullanın.';
+$lang['URL'] = 'URL';
+$lang['Save Settings'] = 'Yapılandırmayı Kaydet';
+$lang['Message text'] = 'Mesaj metni';
+$lang['Fullscreen'] = 'Tam ekran';
+$lang['Common configuration'] = 'Genel yapılandırma';
+$lang['Button text'] = 'Buton metni';
+$lang['Accept'] = 'Kabul et';

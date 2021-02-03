efb67f9bd9033a8028a4c3f707b9727f540e04c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-03 06:46:54 +0100

    Update sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Прихвати';
+$lang['Button text'] = 'Текст дугмета';
+$lang['Common configuration'] = 'Уобичајена подешавања';
+$lang['Full screen overlay'] = 'Распоред преко целог екрана уместо линије у доњем делу.';
+$lang['Fullscreen'] = 'Цео екран';
+$lang['Message text'] = 'Текст поруке';
+$lang['More Info'] = 'Више података';
+$lang['Save Settings'] = 'Сачувај подешавања';
+$lang['URL'] = 'Адреса';
+$lang['Use session cookie'] = 'Котисти колачиће сесије уместо трајних колачића';

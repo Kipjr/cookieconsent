69870df5d999c92cc5aa49a28d7348a400ea498c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-06-11 06:41:32 +0200

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Prijať';
+$lang['Button text'] = 'Text na tlačidle';
+$lang['Common configuration'] = 'Obvyklá konfigurácia';
+$lang['Fullscreen'] = 'Celá obrazovka';
+$lang['Message text'] = 'Text správy';
+$lang['More Info'] = 'Viac informácií';
+$lang['Save Settings'] = 'Uložiť nastavenia';
+$lang['URL'] = 'Adresa URL';
+$lang['Use session cookie'] = 'Použiť cookie sedenia miesto trvalého cookie.';

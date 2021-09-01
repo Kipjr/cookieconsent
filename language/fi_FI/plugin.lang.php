649ff5867ad3cf730259164d25d9a30d0c009434
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-09-01 12:02:13 +0200

    Update fi_FI, thanks to lrd

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Hyväksy';
+$lang['Button text'] = 'Painikkeen teksti';
+$lang['Common configuration'] = 'Yleiset asetukset';
+$lang['Full screen overlay'] = 'Koko ruudun peittotila alareunassa olevan palkin sijaan';
+$lang['Fullscreen'] = 'Koko ruudun tila';
+$lang['Message text'] = 'Viestin teksti';
+$lang['More Info'] = 'Lisää tietoa';
+$lang['Save Settings'] = 'Tallenna Asetukset';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Käytä sessio evästeitä pysyvien sijaan';

c4850fe648e829dd73274bc7914a4093f90173eb
kipjr <martijn9873@hotmail.com>
2021-09-26 23:29:22 +0200

    Fix: Issue with type of cookies not being saved

--- include/cookieconsent.api.php
+++ include/cookieconsent.api.php
@@ -1,18 +1,18 @@
 <?php
-define('PHPWG_ROOT_PATH', '../../../');
+define('PHPWG_ROOT_PATH', dirname(__FILE__, 4) . "/");
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 $array = (array) $data;
 
 global $conf;
+include(PHPWG_ROOT_PATH.'include/common.inc.php');
+
 if($array['consent']==true){
-	
 	if($conf['cookieconsent']['cc_session_cookie']==1) {
-		include(PHPWG_ROOT_PATH.'include/common.inc.php');
 		pwg_set_session_var('cconsent',true);
-	} 
+	}
 	if($conf['cookieconsent']['cc_session_cookie']==0) {
-		include(PHPWG_ROOT_PATH.'include/functions_cookie.inc.php');
+		#include(PHPWG_ROOT_PATH.'include/functions_cookie.inc.php');
 		#pwg_set_cookie_var("cc_persistent_cookie", true, $expire=(time() + (86400 * 365))); #cookie is set for wrong path
 		$fpath = $_SERVER['REQUEST_URI']; #"/piwigo/plugins/cookieconsent/include/cookieconsent.api.php";
 		$cpath = substr($fpath,0,strpos($fpath,"plugins/cookieconsent/include/cookieconsent.api.php")); #returns part before 'plugins', here /piwigo/
@@ -23,6 +23,4 @@ if($array['consent']==true){
 			)
 	        );
 	}
-print_r($conf);
 }
-

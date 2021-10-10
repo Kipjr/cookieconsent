35cbe0a60a5003edc07ac622e538ec7b19c50087
kipjr <martijn9873@hotmail.com>
2021-10-10 16:09:09 +0200

    Add: Additional comments. Remove: obsolete called global vars

--- include/cookieconsent_page.inc.php
+++ include/cookieconsent_page.inc.php
@@ -1,14 +1,14 @@
 <?php
 defined('COOKIECONSENT_PATH') or die('Hacking attempt!');
 
-global $page, $template, $conf, $user, $tokens, $pwg_loaded_plugins, $cc_given;
+global $template, $conf, $cc_given;
 
-
+// when not using session cookie, get user cookie and test if been set. Session cookie is retrieved in main.inc.php and  check server side if consent is given
 if(!isset($conf['cookieconsent']['cc_session_cookie']) || $conf['cookieconsent']['cc_session_cookie'] != 1)  {
-	$cc_given=(pwg_get_cookie_var("cc_persistent_cookie"))==1;
+    $cc_given=(pwg_get_cookie_var("cc_persistent_cookie"))==1;
 }
 
-#bugfix if buttontext is empty.
+// bugfix if buttontext is empty.
 if(!isset($conf['cookieconsent']['cc_accept'])){
 	$conf['cookieconsent']['cc_accept']="Accept";
 }
@@ -17,6 +17,7 @@ else {
 		$conf['cookieconsent']['cc_accept']="Accept";
 	}
 }
+//applying data to template
 $template->assign(array(
   'cookieconsent' => $conf['cookieconsent']
   ));

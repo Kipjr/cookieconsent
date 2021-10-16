074be68676ff0bb5272c17656009d5a2253274ab
Kipjr <martijn@koetsier.dev>
2021-10-16 17:22:00 +0200

    Add: new variables for en_GB

--- language/en_GB/plugin.lang.php
+++ language/en_GB/plugin.lang.php
@@ -9,14 +9,25 @@
 #config
 
 $lang['Common configuration'] = 'Common configuration';
+$lang['Cookie Consent'] = 'Cookie Consent';
+$lang['Persistent Cookie validity'] = 'Persistent Cookie validity';
+$lang['Cookie validity text'] = 'The default validity of the persistent Cookie';
 $lang['Fullscreen'] = 'Fullscreen';
 $lang['Full screen overlay'] = 'Fullscreen overlay instead of a bar in the bottom of screen.';
 $lang['Message text'] = 'Message Text';
-$lang['Button text'] = 'Message Text';
+$lang['Button text'] = 'Button Text';
 $lang['URL'] = 'URL';
+$lang['URL text'] = 'URL text';
+$lang['1 Day'] = '1 Day';
+$lang['7 Days'] = '7 Days';
+$lang['30 Days'] = '30 Days';
+$lang['3 Months'] = '3 Months';
+$lang['6 Months'] = '6 Months';
+$lang['1 Year'] = '1 Year';
 $lang['Save Settings'] = 'Save Settings';
 #popup
 $lang['More Info'] = 'More Info';
-$lang['Accept'] = 'Accept';
+$lang['Session Cookie'] = 'Session Cookie';
+
 
 ?>

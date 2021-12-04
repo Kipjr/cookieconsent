e32367d5a895c3a1417075683f4b507e2e35be3f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-04 13:27:33 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Fullscreen'] = 'Celozaslonski način';
+$lang['Message text'] = 'Besedilo sporočila';
+$lang['More Info'] = 'Več informacije';
+$lang['Save Settings'] = 'Shrani nastavitve';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Uporabite sejni piškotek namesto trajnega piškotka.';
+$lang['Full screen overlay'] = 'Celozaslonsko prekrivanje namesto vrstice na dnu zaslona.';
+$lang['Button text'] = 'Besedilo gumba';
+$lang['Accept'] = 'Sprejmem';
+$lang['Common configuration'] = 'Običajna konfiguracija';

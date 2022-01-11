885a9fd3214362932527df3246bbdfd5183bcca1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-11 13:53:39 +0100

    Update uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use session cookie'] = 'Використовувати файл cookie сеансу замість постійного файлу cookie. ';
+$lang['Save Settings'] = 'Зберегти налаштування';
+$lang['Message text'] = 'Текст повідомлення';
+$lang['Full screen overlay'] = 'Повноекранний оверлей замість смуги в нижній частині екрана. ';
+$lang['Common configuration'] = 'Загальні налаштування';
+$lang['Button text'] = 'Текст кнопки';
+$lang['More Info'] = 'Більше інформації';
+$lang['URL'] = 'URL';
+$lang['Fullscreen'] = 'Повний екран';
+$lang['Accept'] = 'Прийняти';

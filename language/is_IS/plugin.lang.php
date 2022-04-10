8facd9ef3ea02f96a14d8f4ace35f4e9dbb87bb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-10 23:34:07 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Button text'] = 'Texti á hnappi';
+$lang['Accept'] = 'Samþykkja';
+$lang['Common configuration'] = 'Almenn uppsetning';
+$lang['Full screen overlay'] = 'Yfirlögn á öllum skjánum í stað stiku neðst á skjánum.';
+$lang['Fullscreen'] = 'Heilskjár';
+$lang['Message text'] = 'Skilaboðatexti';
+$lang['More Info'] = 'Frekari upplýsingar';
+$lang['Save Settings'] = 'Vista stillingar';
+$lang['URL'] = 'Vefslóði';
+$lang['Use session cookie'] = 'Notaðu lotuköku í stað viðvarandi köku.';

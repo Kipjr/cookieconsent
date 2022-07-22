89711167d629f19690f9a181773a425d7e07d61a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-07-22 06:42:55 +0200

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Acceptera';
+$lang['Button text'] = 'Knapptext';
+$lang['Common configuration'] = 'Allmän konfiguration';
+$lang['Full screen overlay'] = 'Helskärmsöverlägg istället för en stapel längst ner på skärmen.';
+$lang['Fullscreen'] = 'Fullskärm';
+$lang['Message text'] = 'Meddelandetext';
+$lang['More Info'] = 'Mer info';
+$lang['Save Settings'] = 'Spara inställningar';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Använd sessionskaka istället för permanent kaka.';

8ab669f784ecc77a928dfec282c2c45f6d4534d5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-25 12:50:39 +0200

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Full screen overlay'] = 'Překrytí přes celou obrazovku místo pruhu v dolní části obrazovky.';
+$lang['Button text'] = 'Text na tlačítku';
+$lang['Use session cookie'] = 'Použít cookie sezení místo trvalého cookie.';
+$lang['Fullscreen'] = 'Na celou obrazovku';
+$lang['Message text'] = 'Text zprávy';
+$lang['More Info'] = 'Více informací';
+$lang['Save Settings'] = 'Uložit nastavení';
+$lang['URL'] = 'URL';
+$lang['Common configuration'] = 'Běžná konfigurace';
+$lang['Accept'] = 'Přijmout';

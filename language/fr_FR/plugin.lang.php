3b5e0a8cb1215b8151385bc9ab8479ff667b1436
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-16 18:16:05 +0100

    Update fr_FR, thanks to Reville

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -8,13 +8,13 @@
  
 #config
 
-$lang['Common configuration'] = 'Configuration commune';
+$lang['Common configuration'] = 'Configuration standard';
 $lang['Fullscreen'] = 'Plein écran';
 $lang['Full screen overlay'] = "Superposition plein écran au lieu d'une barre en bas de l'écran.";
 $lang['Message text'] = 'texte du message';
 $lang['Button text'] = 'Étiquette de URL';
 $lang['URL'] = 'URL';
-$lang['Save Settings'] = 'enregistrer les paramètres';
+$lang['Save Settings'] = 'Enregistrer les paramètres';
 #popup
 $lang['More Info'] = "Plus d'informations";
 $lang['Accept'] = "J'accepte";

4171ed3e7c5d84173234df76a026c6d18c82c4ac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-09 08:47:17 +0100

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'Aceitar';
+$lang['Button text'] = 'Texto base';
+$lang['Common configuration'] = 'configuração comum';
+$lang['Fullscreen'] = 'Ecrã cheio';
+$lang['Message text'] = 'texto da mensagem';
+$lang['More Info'] = 'Mais informação';
+$lang['Save Settings'] = 'Salvar definições';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = 'Usar cookie de sessão em vez de cookie persistente';

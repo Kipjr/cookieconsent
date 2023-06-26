712baa8dab9860ab20e58dd43b837adbf9c83469
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-06-26 11:07:35 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = '接受';
+$lang['Button text'] = '按鈕文字';
+$lang['Common configuration'] = '一般組態';
+$lang['Full screen overlay'] = '使用全螢幕圖層顯示，而非螢幕下方的資訊列。';
+$lang['Fullscreen'] = '全螢幕';
+$lang['Message text'] = '訊息文字';
+$lang['More Info'] = '更多資訊';
+$lang['Save Settings'] = '儲存設定';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = '使用 session cookie，而非永久性 cookie。';

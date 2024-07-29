9b73cad8b95b67fe2a3fc5a495a2fa5c3ebf55af
Maniues <maniuesquwus@gmail.com>
2024-07-29 13:55:34 +0200

    add licensing header in many files

--- admin.php
+++ admin.php
@@ -1,4 +1,18 @@
 <?php
+/*
++-----------------------------------------------------------------------+
+|                                                                       |
+| Cookie Consent -  Piwigo extension adding cookie consent              |
+| Copyright (C) 2024 Netcie / Kipjr                                     |
+|                                                                       |
+| Cookie Consent is licensed under the GNU General Public License       |
+| version 2 or (at your option) any later version, as is Piwigo itself. |
+|                                                                       |
+| For more details, check the COPYING or LICENSE file in the top-level  |
+| directory of this extension.                                          |
+|                                                                       |
++-----------------------------------------------------------------------+
+*/
 /**
  * This is the main administration page, if you have only one admin page you can put
  * directly its code here or using the tabsheet system like bellow
--- template/index.php
+++ template/index.php
@@ -1,4 +1,18 @@
 <?php
+/*
++-----------------------------------------------------------------------+
+|                                                                       |
+| Cookie Consent -  Piwigo extension adding cookie consent              |
+| Copyright (C) 2024 Netcie / Kipjr                                     |
+|                                                                       |
+| Cookie Consent is licensed under the GNU General Public License       |
+| version 2 or (at your option) any later version, as is Piwigo itself. |
+|                                                                       |
+| For more details, check the COPYING or LICENSE file in the top-level  |
+| directory of this extension.                                          |
+|                                                                       |
++-----------------------------------------------------------------------+
+*/
 $url = '../';
 header( 'Request-URI: '.$url );
 header( 'Content-Location: '.$url );

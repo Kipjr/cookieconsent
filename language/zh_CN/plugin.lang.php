646cadde2e301655fff3c915d9057866bff8b469
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:22:47 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = '接受';
+$lang['Button text'] = '按钮文本';
+$lang['Common configuration'] = '通用配置';
+$lang['Full screen overlay'] = '全屏覆盖，而不是屏幕底部的栏。';
+$lang['Fullscreen'] = '全屏';
+$lang['Message text'] = '消息文本';
+$lang['More Info'] = '更多信息';
+$lang['Save Settings'] = '保存设置';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = '使用会话cookie 而不是持久cookie。';

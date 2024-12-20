2dc5840fffc0f89c6c42cdeafac739ff643cab98
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:54:49 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = 'קבל';
+$lang['Button text'] = 'טקסט לכפתור';
+$lang['Common configuration'] = 'הגדרה רגילה';
+$lang['Full screen overlay'] = 'כיסוי מסך מלא במקום סרגל בתחתית המסך.';
+$lang['Fullscreen'] = 'מסך מלא';
+$lang['Message text'] = 'טקסט הודעה';
+$lang['More Info'] = 'מידע נוסף';
+$lang['Save Settings'] = 'שמור הגדרות';
+$lang['URL'] = 'כתובת URL';
+$lang['Use session cookie'] = 'השתמש בעוגיית סשן במקום עוגיית קוקי קבועה.';

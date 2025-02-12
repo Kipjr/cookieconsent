19f6449ae30830aa6f62fa363efc042a380c6559
max-ima <96241406+max-ima@users.noreply.github.com>
2025-02-12 13:52:54 +0100

    Update plugin.lang.php

--- language/sl_SI/plugin.lang.php
+++ language/sl_SI/plugin.lang.php
@@ -5,13 +5,25 @@
 // | For copyright and license information, please view the COPYING.txt    |
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
-$lang['Fullscreen'] = 'Celozaslonski način';
-$lang['Message text'] = 'Besedilo sporočila';
-$lang['More Info'] = 'Več informacije';
-$lang['Save Settings'] = 'Shrani nastavitve';
-$lang['URL'] = 'URL';
-$lang['Use session cookie'] = 'Uporabite sejni piškotek namesto trajnega piškotka.';
-$lang['Full screen overlay'] = 'Celozaslonsko prekrivanje namesto vrstice na dnu zaslona.';
-$lang['Button text'] = 'Besedilo gumba';
-$lang['Accept'] = 'Sprejmem';
+#config
 $lang['Common configuration'] = 'Običajna konfiguracija';
+$lang['Cookie Consent'] = 'Soglasje za piškotke';
+$lang['Persistent Cookie validity'] = 'Veljavnost trajnih piškotkov';
+$lang['Use session cookie'] = 'Uporabite sejni piškotek namesto trajnega piškotka.';
+$lang['Cookie validity text'] = 'Privzeta veljavnost trajnega piškotka';
+$lang['Fullscreen'] = 'Celozaslonski način';
+$lang['Full screen overlay'] = 'Celozaslonsko prekrivanje namesto vrstice na dnu zaslona.';
+$lang['Message text'] = 'Besedilo sporočila';
+$lang['URL'] = 'URL';
+$lang['URL text'] = 'Besedilo URL-ja';
+$lang['1 Day'] = '1 Dan';
+$lang['7 Days'] = '7 Dni';
+$lang['30 Days'] = '30 Dni';
+$lang['3 Months'] = '3 Mesece';
+$lang['6 Months'] = '6 Mesecev';
+$lang['1 Year'] = '1 Leto';
+$lang['Save Settings'] = 'Shrani nastavitve';
+$lang['Session Cookie'] = 'Sejni piškotek';
+#popup
+$lang['More Info'] = 'Več informacije';
+$lang['Accept'] = 'Besedilo gumba';

f6d6f6de5e28e268d3d71dc81df869f1f13db636
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-05 06:20:37 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Accept'] = '동의';
+$lang['Button text'] = '버튼 텍스트';
+$lang['Common configuration'] = '일반적인 환경 설정';
+$lang['Full screen overlay'] = '화면 하단의 바 대신 전체 화면 오버레이.';
+$lang['Fullscreen'] = '전체 화면';
+$lang['Message text'] = '메시지 텍스트';
+$lang['More Info'] = '추가 정보';
+$lang['Save Settings'] = '설정 저장';
+$lang['URL'] = 'URL';
+$lang['Use session cookie'] = '영구 쿠키 대신 세션 쿠키를 사용합니다.';
